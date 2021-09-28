45d6e93fbb6a50c5656c2095bfd0f76c67d62f73
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2021-09-28 15:09:58 +0200

    almost 200fps ;-)

--- cpp/tetris-table/src/main.cpp
+++ cpp/tetris-table/src/main.cpp
@@ -10,13 +10,17 @@ void setup() {
 }
 
 void loop() {
+  unsigned long start = millis();
   for (int i=0; i<150; i++) {
-    Serial.printf("x=%d, y=%d\n", i%10, i/10);
+    //Serial.printf("x=%d, y=%d\n", i%10, i/10);
     t.setPixel(i%10, i/10, t.color(i*13%256, (i*17+100)%256, (i*28+200)%256));
     t.show();
-    delay(100);
+    //delay(10);
     t.setButtonLEDs(1 << (i%8));
-    Serial.println(t.getButtons());
+    //Serial.println(t.getButtons());
   }
+  start = millis()-start;
+  Serial.printf("150 cycles in %ld ms -> %f fps\n", start, (1000*150.0/start));
   t.pixels->clear();
+  
 }
